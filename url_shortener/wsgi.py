fb6d6c69ba1a5f52c46418d686bea185e4826b49
Shuvrajyoti <shuvra458@gmail.com>
2025-08-07 19:32:35 +0530

    Update wsgi.py

--- url_shortener/wsgi.py
+++ url_shortener/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "url_shortener.settings")
+django.setup()
+
+#----Auto Migration----
+from django.core.management import call_command
+try:
+  call_command('migrate', interactive=False)
+except Exception as e:
+  print(f"Migration error:{e}")
 
 application = get_wsgi_application()
